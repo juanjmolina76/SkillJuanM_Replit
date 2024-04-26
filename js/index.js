f3b64186653a19537e5dce822d98b177625ac363
juanjmolina76 <108595263+juanjmolina76@users.noreply.github.com>
2024-04-26 17:35:30 +0000

    agrego prompt  calculo año nac segun edad de js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log("hola");
+
+resultado = prompt("Dime tu nombre");
+
+document.write(`<h1 class="grad">Hola ${resultado}</h1>`);
+
+const edad = (d_edad=prompt("Dime tu edad")) =>{
+    document.write(`<p>${resultado} nació el año</p>` + (2024 - d_edad));
+  }
+edad();
+
+/*
+console.log(resultado);
+let name=("juan");
+console.log(name);
+*/
+
+
+// document.write(`<h1 class="grad">Hola ${resultado}</h1>`);
+
+// document.write(`<h1>
