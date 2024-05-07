d1105227064552e36164c36c7cdbb604a00f18a5
juanjmolina76 <108595263+juanjmolina76@users.noreply.github.com>
2024-05-07 09:34:57 +0000

    mejoro h2 del script

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log("hola");
 
 resultado = prompt("Dime tu nombre");
 
-document.write(`<h1 class="grad">Hola ${resultado}</h1>`);
+document.write(`<h2 class="grad">Hola ${resultado}</h2>`);
 
 const edad = (d_edad=prompt("Dime tu edad")) =>{
     document.write(`<p>${resultado} nació el año</p>` + (2024 - d_edad));
