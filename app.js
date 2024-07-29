d9305cc6f71c0dd8ce87b390643c914ee37e2de3
juanmolina76 <molinajuanj@hotmail.com>
2024-07-29 19:15:34 -0300

    paso todo a estatico / dinamico SIN rutas solo entry point

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = express()
+const express = require(`express`)
+const app = express()
 
 const port = 8080 || process.env.PORT || 3000
 
-const app = express()
-app.get ("/", (req, res) => {
-    app.sendFile (__dirname + '/proyectos.html')
+app.use(express.static(__dirname + '/public')); //2do indico dónde buscará css, js,etc de los archivos estaticos
+//igualmente al llamarse index al ser static public, busca el primer archivo estatico index
+app.get ("/perfil", (req, res) => {
+    res.sendFile (__dirname + '/public/perfil.html') // 1ero dirijo e pedido "/" al index
 
 })
 
-app.listen (port, () => console.log('Estoy arriba en el puerto ${port}'))
+app.get ("/listado", (req, res) => {
+    res.sendFile (__dirname + '/src/view/proyectos.html') // 1ero dirijo e pedido "/ al index
+})
+
+
+
+
+
+app.listen (port, () => console.log(`Estoy arriba en el puerto ${port}`))
 
