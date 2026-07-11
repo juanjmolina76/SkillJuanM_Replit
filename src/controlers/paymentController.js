f6161b29d8e5ca712ae93280e753ea3fdb23c78f
juanjmolina76 <molianjuanj@hotmail.com>
2026-07-11 09:42:03 -0300

    modifico paymentController lee orderService

--- src/controlers/paymentController.js
+++ src/controlers/paymentController.js
@@ -1,15 +1,27 @@
-const { Order } = require("mercadopago");
+const orderService = require('../services/orderService');
 
-function success(req, res) {
-    res.render('success', {
-        Order
-        //query: req.query <p><%= query.status %></p>
+async function success(req, res) {
+    try {
+
+        const order = await orderService.getOrderById(
+            req.query.external_reference
+        );
+
+        res.render('success', {
+            order
+
+                    //query: req.query <p><%= query.status %></p>
 /*
         paymentId: req.query.payment_id,
         orderId: req.query.external_reference,
         status: req.query.status
 */
-    });
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error obteniendo la orden.");
+    }
 }
 
 function pending(req, res) {
